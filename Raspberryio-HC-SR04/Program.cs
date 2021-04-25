bcbe97192319a208139796e518dc5aa807e8d3dc
Daniel Silvestre <danielsilvestre.work@gmail.com>
2021-04-25 10:46:10 +0200

    First commit

--- Raspberryio-HC-SR04/Program.cs
+++ Raspberryio-HC-SR04/Program.cs
@@ -34,7 +34,7 @@ namespace Raspberryio_HC_SR04
             Hcsr04 hcsr04 = new Hcsr04(Pi.Gpio[P1.Gpio23], Pi.Gpio[P1.Gpio24]);
             hcsr04.Start();
 
-            Console.WriteLine("Program will read the distance from HC-SR04 every second until you cancel it by CTRL+C");
+            Console.WriteLine("Program will read the distance from HC-SR04 every 500 ms until you cancel it by CTRL+C");
             while (Running)
             {
                 Console.WriteLine("Distance : " + (hcsr04.Distance!=null ? hcsr04.Distance + "cm" : "No obstacle detected or too close"));
